11677cd63abaa8b9f30cfcf49c11ab1d38673794
boundary-x <hi@boundaryx.io>
2024-12-19 15:53:44 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -575,19 +575,3 @@ namespace neopixel {
         Shortest
     }
 }
-
-namespace ws2812b {
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }
-
-    //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalPin, mode: number) {
-
-    }
-
-    export const BUFFER_MODE_RGB = 1
-    export const BUFFER_MODE_RGBW = 2
-    export const BUFFER_MODE_RGB_RGB = 3
-    export const BUFFER_MODE_AP102 = 4
-}
